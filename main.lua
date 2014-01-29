80b5c06b88de385833286ac5d7d3d758995b91e2
xmorg <tcooper@SDLCNU3349GWV.corp.lpl.com>
2014-01-29 15:57:23 -0800

    added invisible buttons, buggy navigation.

--- main.lua
+++ main.lua
@@ -1,7 +1,10 @@
 game = { 
 	tilecount = 80, 
-	draw_x = love.graphics.getWidth()/2 / 8 - 1, --starts drawing at 0,0
-	draw_y = love.graphics.getHeight()/2 / 14 -2
+	drawcount = 50,
+	draw_x = 1, --starts drawing at 0,0
+	draw_y = 1,
+	loc_x,
+	loc_y
 }
 temp_map = {}
 obj_map = {}
@@ -24,6 +27,24 @@ function love.load()
 	end
 end
 
+function love.mousepressed(x, y, button)
+	if button == "l" then
+		if x >=0 and x <=100 and y >= love.graphics.getHeight()/10 
+			and y <= love.graphics.getHeight()/10*9 then
+			game.draw_x = game.draw_x +1
+		elseif x >=love.graphics.getWidth()/10 and x <=love.graphics.getWidth() 
+			and y >= love.graphics.getHeight()/10 
+			and y <= love.graphics.getHeight()/10*9 then
+			game.draw_x = game.draw_x -1
+		elseif x >=love.graphics.getWidth()/10 and x <=love.graphics.getWidth()/10*9 
+			and y >= 0 and y <= 100 then
+			game.draw_y = game.draw_y +1
+		elseif x >=love.graphics.getWidth()/10 and x <=love.graphics.getWidth()/10*9 
+			and y <= love.graphics.getHeight() and y >= love.graphics.getHeight()/10*9 then
+			game.draw_y = game.draw_y -1
+		end--endif
+	end--endif
+end
 function love.keypressed( key, isrepeat )
 	if key == "escape" then
 		love.event.quit()
@@ -50,20 +71,43 @@ function setcolorbyChar(char)
 		return 255,0,0,255
 	end
 end
-
-function love.draw_cam_viewable(actor)
+function love.draw_border()
+	characters_x = love.graphics.getWidth() / 8
+	characters_y = love.graphics.getHeight()/ 14
+	love.graphics.setColor(255,255,255,255)
+	love.graphics.print("+", 0, 0)
+	love.graphics.print("+", love.graphics.getWidth()-8, 0)
+	love.graphics.print("+", 0, love.graphics.getHeight()-14)
+	
+	for y=1, characters_y do
+		love.graphics.print("|", 0,y*14)
+		love.graphics.print("|", love.graphics.getWidth()-8, y*14)
+	end
+	for x=1, characters_x do
+		love.graphics.print("-", x*8,0)
+		love.graphics.print("-", x*8,love.graphics.getHeight()-14)
+		love.graphics.print("-", x*8, 28)
+	end
+end
+function love.draw_cam_viewable(lx,ly)
 	local draw_center_x = love.graphics.getWidth()/2
 	local draw_center_y = love.graphics.getHeight()/2
 
-	for y=1 , game.tilecount do
-		for x=1 , game.tilecount do
-			love.graphics.setColor(setcolorbyChar(temp_map[y][x]))
-			love.graphics.print(temp_map[y][x],x*8+game.draw_x*8,y*14+game.draw_y*14)
+	for y=1 , game.drawcount do --50
+		for x=1 , game.drawcount do --50
+			if lx*x > 1 and lx*x < game.tilecount and ly*y > 1 and ly*y < game.tilecount then
+				love.graphics.setColor(setcolorbyChar(temp_map[y][x]))
+				love.graphics.print(temp_map[y*ly][x*lx],x*8,y*14)
+			end
+			--love.draw_map_by_char()
+			--love.graphics.setColor(setcolorbyChar(temp_map[y][x]))
+			--love.graphics.print(temp_map[y][x],x*8+game.draw_x*8,y*14+game.draw_y*14)
 		end
 	end
 	love.graphics.setColor(255,255,255,255)
-	love.graphics.print(actor, draw_center_x-4,draw_center_y-7 )
+	love.graphics.print("@", draw_center_x-4,draw_center_y-7 )
+	love.draw_border()
 end
 function love.draw()
-	love.draw_cam_viewable("@")
+	love.draw_cam_viewable(game.draw_x, game.draw_y)
 end

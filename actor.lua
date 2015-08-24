a598c62c6f556f7fffcf910382a29fb1e9445b66
xmorg <jobs@cooperlabs.net>
2015-08-24 13:35:00 -0700

    Update actor.lua

--- actor.lua
+++ actor.lua
@@ -129,11 +129,11 @@ function display_actor_stats(actor, editing)--actor object, boolean viewable
 		love.graphics.print(actor.agility,coll_one+100, line_num*14) line_num=line_num+1
 	love.graphics.print(point_at_current_stat(7).." Intelligence:", coll_one, line_num*14)
 		love.graphics.print( actor.intel,coll_one+100, line_num*14) line_num=line_num+1
-	love.graphics.print(point_at_current_stat(8).."Stamina:", coll_one, line_num*14)
+	love.graphics.print(point_at_current_stat(8).." Stamina:", coll_one, line_num*14)
 		love.graphics.print( actor.stamina, coll_one+100, line_num*14) line_num=line_num+1
-	love.graphics.print(point_at_current_stat(9).."Charisma:", coll_one, line_num*14)
+	love.graphics.print(point_at_current_stat(9).." Charisma:", coll_one, line_num*14)
 		love.graphics.print( actor.charisma, coll_one+100, line_num*14) line_num=line_num+1
-	love.graphics.print(point_at_current_stat(10).."Luck:", coll_one, line_num*14)
+	love.graphics.print(point_at_current_stat(10).." Luck:", coll_one, line_num*14)
 		love.graphics.print( actor.luck, coll_one+100, line_num*14) line_num=line_num+1
 end
 function r_gen_background(a, rand_race)
@@ -177,8 +177,8 @@ function randomize_actor(a, race)
 	return a
 end
 function point_at_current_stat(s)
-	if s == a.current_stat then return ">>"
-	else return "  " end
+	if s == a.current_stat then return "(*)"
+	else return "( )" end
 end
 function update_actor_chargen(a, key, mouse_B, mouse_x, mouse_y) --updates based on mouse/key press
 	if key == "r" then  --randomize actor

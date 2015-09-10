69355f7777d7827ac354060ed728cc1a63e92121
xmorg <jobs@cooperlabs.net>
2015-09-09 18:15:50 -0700

    Update interact.lua

--- interact.lua
+++ interact.lua
@@ -1,3 +1,10 @@
 --interact .lua, talk, look, etc
 function on_interact(player, o)
 end
+
+function get_obstacle(c) --test weather character is an obstacle
+  if c == "#" then return 1
+  elseif c == "t" then return 1
+  elseif c == "l" then return 1
+  else return 0
+end

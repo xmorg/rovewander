8e108477bc46dfb95d662b7e0471bbb4672a3f21
xmorg <jobs@cooperlabs.net>
2015-09-10 11:13:48 -0700

    Update interact.lua

--- interact.lua
+++ interact.lua
@@ -1,5 +1,12 @@
 --interact .lua, talk, look, etc
-
+function get_outofbounds(y,x)
+  if y<1 or y > game.tilecount or x<1 or x >game.tilecount then
+    return 1
+  else
+    return 0
+  end
+  
+end
 function on_get_obstacle_look(p) -- see what is blocking your path
   if player.facing == "north" then
     show_look_data(game_map[game.player_loc_y-1][game.player_loc_x], game.player_loc_x, game.player_loc_y-1)
@@ -21,14 +28,16 @@ function get_ncpmap_obstacle(y,x) --check if npc_map returned an obstacle
 end
 
 function get_obstacle(y,x) --test weather character is an obstacle
-  if game_map[y][x] == "#" then 
-    return 1
-  elseif game_map[y][x] == "t" then 
-    return 1
-  elseif game_map[y][x] == "l" then 
-    return 1
-  else 
-    return 0
+  if get_outofbounds(y,x) == 0 then
+    if game_map[y][x] == "#" then 
+      return 1
+    elseif game_map[y][x] == "t" then 
+      return 1
+    elseif game_map[y][x] == "l" then 
+      return 1
+    else 
+      return 0
+    end
   end
 end
 

7ee7cb481322204883da3b77b1fa1f1149be4d2d
xmorg <jobs@cooperlabs.net>
2015-09-09 18:31:34 -0700

    Update interact.lua

--- interact.lua
+++ interact.lua
@@ -2,28 +2,28 @@
 function on_interact(player, o)
 end
 
-function get_ncpmap_obstacle(c) --check if npc_map returned an obstacle
-  if c == "@" then 
+function get_ncpmap_obstacle(y,x) --check if npc_map returned an obstacle
+  if npc_map[y][x] == "@" then 
     return 1
   else 
     return 0
   end
 end
 
-function get_obstacle(c) --test weather character is an obstacle
-  if c == "#" then 
+function get_obstacle(y,x) --test weather character is an obstacle
+  if game_map[y][x] == "#" then 
     return 1
-  elseif c == "t" then 
+  elseif game_map[y][x] == "t" then 
     return 1
-  elseif c == "l" then 
+  elseif game_map[y][x] == "l" then 
     return 1
   else 
     return 0
   end
 end
 
-function get_any_obstacle(c, n)
-  if get_obstacle(c) == 1 or get_ncpmap_obstacle(n) == 1 then
+function get_any_obstacle(y,x)
+  if get_obstacle(y,x) == 1 or get_ncpmap_obstacle(y,x) == 1 then
     return 1
   else
     return 0

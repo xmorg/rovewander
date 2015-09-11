6a77a2805d003871307005ab895bc953d36dd69c
xmorg <jobs@cooperlabs.net>
2015-09-10 19:43:42 -0700

    Update interact.lua

--- interact.lua
+++ interact.lua
@@ -28,7 +28,7 @@ function get_ncpmap_obstacle(y,x) --check if npc_map returned an obstacle
 end
 
 function get_obstacle(y,x) --test weather character is an obstacle
-  --if get_outofbounds(y,x) == 0 then
+  if get_outofbounds(y,x) == 0 then
     if game_map[y][x] == "#" then 
       return 1
     elseif game_map[y][x] == "t" then 
@@ -38,7 +38,7 @@ function get_obstacle(y,x) --test weather character is an obstacle
     else 
       return 0
     end
-  --end
+  end
 end
 
 function get_any_obstacle(y,x)

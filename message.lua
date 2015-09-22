36fb3a8802e91b4970bc8beb5da2094d173fda19
xmorg <jobs@cooperlabs.net>
2015-09-22 15:49:39 -0700

    Update message.lua

--- message.lua
+++ message.lua
@@ -18,7 +18,7 @@ function draw_border_m(r, g, b, a)
 	--tl = 20
 	love.graphics.setColor(r,g,b,a)
 	love.graphics.rectangle("fill", tl,tl, 400, 200) --top
-	love.graphics.setColor(0,0,0,255)
+	love.graphics.setColor(0,0,0,0)
 	love.graphics.rectangle("fill", tl+8,tl+14, 400-16, 200-28)
 end
 
